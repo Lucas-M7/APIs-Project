a8d694697ae69dc2f486864aca5f55b2cf2a0b6a
Lucas Mateus <Lucas015x@gmail.com>
2024-03-26 18:30:55 -0300

    feat: Adicionando API para buscar o CEP

--- Controllers/APICep/SearchCepController.cs
+++ Controllers/APICep/SearchCepController.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Api_Que_ConsomeOutrasApi.Controllers.APICep;
 
@@ -12,6 +13,34 @@ public class SearchCepController : ControllerBase
     public SearchCepController(IHttpClientFactory httpClientFactory)
     {
         _httpClient = httpClientFactory.CreateClient();
-        _httpClient.BaseAddress = new Uri("https://cdn.apicep.com/file/apicep/{cep}.json");
+        _httpClient.BaseAddress = new Uri("https://viacep.com.br/");
+    }
+
+    [HttpGet("GetYourCep/{UF}/{Cidade}/{Bairro}")]
+    public async Task<IActionResult> GetYoutCep(string UF, string Cidade, string Bairro)
+    {
+        try
+        {
+            string apiUrl = $"https://viacep.com.br/ws/{UF}/{Cidade}/{Bairro}/json/";
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+
+                var myCep = JsonConvert.DeserializeObject<object>(content);
+                string formattedJson = JsonConvert.SerializeObject(myCep, Formatting.Indented);
+
+                return Ok(formattedJson);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+            }
+        }
+        catch
+        {
+            throw new ArgumentException("Falha ao fazer o consumo da API");
+        }
     }
 }
--- Controllers/FipeVeiculos/VeiculosController.cs
+++ Controllers/FipeVeiculos/VeiculosController.cs
@@ -42,34 +42,7 @@ public class VeiculosController : ControllerBase
         }
     }
 
-    [HttpGet("motos/marcas")]
-    public async Task<IActionResult> GetBikers()
-    {
-        try
-        {
-            string apiUrl = "https://parallelum.com.br/fipe/api/v1/motos/marcas";
-
-            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
-
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                var veiculos = JsonConvert.DeserializeObject<object>(content);
-                string formattedJson = JsonConvert.SerializeObject(veiculos, Formatting.Indented);
-                return Ok(formattedJson);
-            }
-            else
-            {
-                return StatusCode((int)response.StatusCode, response.ReasonPhrase);
-            }
-        }
-        catch
-        {
-            throw new ArgumentException("Falha ao fazer o consumo da API");
-        }
-    }
-
-    [HttpGet("carros/marcas{id}/modelos")]
+    [HttpGet("carros/marcas/{id}/modelos")]
     public async Task<IActionResult> GetCarsModel(int id)
     {
         try
